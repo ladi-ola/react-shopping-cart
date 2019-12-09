fd661fc6509ce75153a742d3eb9a7adaf134dbe8
oredola <oredolaolamide@gmail.com>
2019-12-09 14:57:08 +0100

    Refactor: Navigation now using Context API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from "./components/ShoppingCart";
 
 //Contexts
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -21,11 +22,14 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={cart}>
 
-        <Route exact path="/" component={Products} />
+          <Navigation />
+					
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
 
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
